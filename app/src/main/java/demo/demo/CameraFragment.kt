92f2b74bfa8d075ecb6f8bf3bf2f8441f4750dbe
Suvidha Patekar <suvidha.patekar@myglamm.com>
2018-09-15 15:29:34 +0530

    Initial commit

---
+++ app/src/main/java/demo/demo/CameraFragment.kt
@@ -0,0 +1,38 @@
+package demo.demo
+
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CameraFragment : Fragment() {
+  private var listener: OnFragmentInteractionListener? = null
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+  }
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_camera, container, false)
+  }
+
+  interface OnFragmentInteractionListener {
+    // TODO: Update argument type and name
+    fun onFragmentInteraction(uri: Uri)
+  }
+
+  companion object {
+    @JvmStatic fun newInstance() =
+      CameraFragment().apply {
+        arguments = Bundle().apply {
+        }
+      }
+  }
+}
---
+++ app/src/main/java/demo/demo/MainActivity.kt
@@ -0,0 +1,75 @@
+package demo.demo
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.toolbar
+
+class MainActivity : AppCompatActivity() {
+  private val PERMISSIONS_REQUEST_CODE = 200
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    setSupportActionBar(toolbar)
+    getPermission()
+  }
+
+  private fun getPermission() {
+    if (ContextCompat.checkSelfPermission(
+            this, Manifest.permission.CAMERA
+        ) != PackageManager.PERMISSION_GRANTED ||
+        ContextCompat.checkSelfPermission(
+            this, Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) != PackageManager.PERMISSION_GRANTED
+    ) {
+      ActivityCompat.requestPermissions(
+          this,
+          arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE),
+          PERMISSIONS_REQUEST_CODE
+      )
+    }
+  }
+
+  override fun onRequestPermissionsResult(
+    requestCode: Int,
+    permissions: Array<out String>,
+    grantResults: IntArray
+  ) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    when (requestCode) {
+      PERMISSIONS_REQUEST_CODE -> {
+        if ((grantResults.isNotEmpty())) {
+          if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+          } else {
+            showToast(R.string.need_permission)
+            getPermission()
+          }
+
+          if (grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+          } else {
+            showToast(R.string.need_permission)
+            getPermission()
+          }
+        } else {
+
+        }
+        return
+      }
+    }
+  }
+
+  private fun showToast(message: Int) {
+    val toast = Toast.makeText(
+        this, message, Toast.LENGTH_LONG
+    )
+    toast.show()
+  }
+}
+
